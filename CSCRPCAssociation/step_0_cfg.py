8a3b49d9dbc63023b1542932b1ab36f53985b918
Joao Pinheiro <joaopedro.fisicauerj@gmail.com>
2021-06-22 14:47:24 +0200

    Adding the step0 from Sandro recipe

--- CSCRPCAssociation/step_0_cfg.py
+++ CSCRPCAssociation/step_0_cfg.py
@@ -26,62 +26,62 @@ process.load('Configuration.StandardSequences.EndOfProcess_cff')
 process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_cff')
 
 process.maxEvents = cms.untracked.PSet(
-    input = cms.untracked.int32(1000),
-    output = cms.optional.untracked.allowed(cms.int32,cms.PSet)
+	input = cms.untracked.int32(1000),
+	output = cms.optional.untracked.allowed(cms.int32,cms.PSet)
 )
 # Input source
 process.source = cms.Source("EmptySource")
 
 process.options = cms.untracked.PSet(
-    FailPath = cms.untracked.vstring(),
-    IgnoreCompletely = cms.untracked.vstring(),
-    Rethrow = cms.untracked.vstring(),
-    SkipEvent = cms.untracked.vstring(),
-    allowUnscheduled = cms.obsolete.untracked.bool,
-    canDeleteEarly = cms.untracked.vstring(),
-    deleteNonConsumedUnscheduledModules = cms.untracked.bool(True),
-    emptyRunLumiMode = cms.obsolete.untracked.string,
-    eventSetup = cms.untracked.PSet(
-        forceNumberOfConcurrentIOVs = cms.untracked.PSet(
-            allowAnyLabel_=cms.required.untracked.uint32
-        ),
-        numberOfConcurrentIOVs = cms.untracked.uint32(1)
-    ),
-    fileMode = cms.untracked.string('FULLMERGE'),
-    forceEventSetupCacheClearOnNewRun = cms.untracked.bool(False),
-    makeTriggerResults = cms.obsolete.untracked.bool,
-    numberOfConcurrentLuminosityBlocks = cms.untracked.uint32(1),
-    numberOfConcurrentRuns = cms.untracked.uint32(1),
-    numberOfStreams = cms.untracked.uint32(0),
-    numberOfThreads = cms.untracked.uint32(1),
-    printDependencies = cms.untracked.bool(False),
-    sizeOfStackForThreadsInKB = cms.optional.untracked.uint32,
-    throwIfIllegalParameter = cms.untracked.bool(True),
-    wantSummary = cms.untracked.bool(False)
+	FailPath = cms.untracked.vstring(),
+	IgnoreCompletely = cms.untracked.vstring(),
+	Rethrow = cms.untracked.vstring(),
+	SkipEvent = cms.untracked.vstring(),
+	allowUnscheduled = cms.obsolete.untracked.bool,
+	canDeleteEarly = cms.untracked.vstring(),
+	deleteNonConsumedUnscheduledModules = cms.untracked.bool(True),
+	emptyRunLumiMode = cms.obsolete.untracked.string,
+	eventSetup = cms.untracked.PSet(
+		forceNumberOfConcurrentIOVs = cms.untracked.PSet(
+			allowAnyLabel_=cms.required.untracked.uint32
+		),
+		numberOfConcurrentIOVs = cms.untracked.uint32(1)
+	),
+	fileMode = cms.untracked.string('FULLMERGE'),
+	forceEventSetupCacheClearOnNewRun = cms.untracked.bool(False),
+	makeTriggerResults = cms.obsolete.untracked.bool,
+	numberOfConcurrentLuminosityBlocks = cms.untracked.uint32(1),
+	numberOfConcurrentRuns = cms.untracked.uint32(1),
+	numberOfStreams = cms.untracked.uint32(0),
+	numberOfThreads = cms.untracked.uint32(8),
+	printDependencies = cms.untracked.bool(False),
+	sizeOfStackForThreadsInKB = cms.optional.untracked.uint32,
+	throwIfIllegalParameter = cms.untracked.bool(True),
+	wantSummary = cms.untracked.bool(False)
 )
 # Production Info
 process.configurationMetadata = cms.untracked.PSet(
-    annotation = cms.untracked.string('SingleMuPt100_Eta2p85_cfi nevts:10'),
-    name = cms.untracked.string('Applications'),
-    version = cms.untracked.string('$Revision: 1.19 $')
+	annotation = cms.untracked.string('SingleMuPt100_Eta2p85_cfi nevts:10'),
+	name = cms.untracked.string('Applications'),
+	version = cms.untracked.string('$Revision: 1.19 $')
 )
 
 # Output definition
 
 process.RAWSIMoutput = cms.OutputModule("PoolOutputModule",
-    SelectEvents = cms.untracked.PSet(
-        SelectEvents = cms.vstring('generation_step')
-    ),
-    compressionAlgorithm = cms.untracked.string('LZMA'),
-    compressionLevel = cms.untracked.int32(1),
-    dataset = cms.untracked.PSet(
-        dataTier = cms.untracked.string('GEN-SIM'),
-        filterName = cms.untracked.string('')
-    ),
-    eventAutoFlushCompressedSize = cms.untracked.int32(20971520),
-    fileName = cms.untracked.string('file:step0.root'),
-    outputCommands = process.RAWSIMEventContent.outputCommands,
-    splitLevel = cms.untracked.int32(0)
+	SelectEvents = cms.untracked.PSet(
+		SelectEvents = cms.vstring('generation_step')
+	),
+	compressionAlgorithm = cms.untracked.string('LZMA'),
+	compressionLevel = cms.untracked.int32(1),
+	dataset = cms.untracked.PSet(
+		dataTier = cms.untracked.string('GEN-SIM'),
+		filterName = cms.untracked.string('')
+	),
+	eventAutoFlushCompressedSize = cms.untracked.int32(20971520),
+	fileName = cms.untracked.string('file:step0.root'),
+	outputCommands = process.RAWSIMEventContent.outputCommands,
+	splitLevel = cms.untracked.int32(0)
 )
 
 # Other statements
@@ -90,24 +90,23 @@ from Configuration.AlCa.GlobalTag import GlobalTag
 process.GlobalTag = GlobalTag(process.GlobalTag, 'auto:phase2_realistic_T21', '')
 
 process.generator = cms.EDFilter("Pythia8PtGun",
-    PGunParameters = cms.PSet(
-        AddAntiParticle = cms.bool(True),
-        MaxEta = cms.double(2.85),
-        MaxPhi = cms.double(3.14159265359),
-        MaxPt = cms.double(100.01),
-        MinEta = cms.double(-2.85),
-        MinPhi = cms.double(-3.14159265359),
-        MinPt = cms.double(99.99),
-        ParticleID = cms.vint32(-13)
-    ),
-    PythiaParameters = cms.PSet(
-        parameterSets = cms.vstring()
-    ),
-    Verbosity = cms.untracked.int32(0),
-    firstRun = cms.untracked.uint32(1),
-    psethack = cms.string('single mu pt 100')
+	PGunParameters = cms.PSet(
+		AddAntiParticle = cms.bool(True),
+		MaxEta = cms.double(2.85),
+		MaxPhi = cms.double(3.14159265359),
+		MaxPt = cms.double(100.01),
+		MinEta = cms.double(-2.85),
+		MinPhi = cms.double(-3.14159265359),
+		MinPt = cms.double(99.99),
+		ParticleID = cms.vint32(-13)
+	),
+	PythiaParameters = cms.PSet(
+		parameterSets = cms.vstring()
+	),
+	Verbosity = cms.untracked.int32(0),
+	firstRun = cms.untracked.uint32(1),
+	psethack = cms.string('single mu pt 100')
 )
-
 
 # Path and EndPath definitions
 process.generation_step = cms.Path(process.pgen)
@@ -128,7 +127,7 @@ process.options.eventSetup.numberOfConcurrentIOVs = 1
 if hasattr(process, 'DQMStore'): process.DQMStore.assertLegacySafe=cms.untracked.bool(False)
 # filter all path with the production filter sequence
 for path in process.paths:
-        getattr(process,path).insert(0, process.generator)
+	getattr(process,path).insert(0, process.generator)
 
 # customisation of the process.
 
@@ -146,5 +145,3 @@ from Configuration.StandardSequences.earlyDeleteSettings_cff import customiseEar
 process = customiseEarlyDelete(process)
 # End adding early deletion
                                                                                                                             154,1         Bot
-
-
