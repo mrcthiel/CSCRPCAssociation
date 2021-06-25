d504b7bbd5febde520bad1f5424b395e95beb319
Joao Pinheiro <joaopedro.fisicauerj@gmail.com>
2021-06-25 16:35:06 +0200

    Add the config file

--- CSCRPCAssociation/cscrpc_reco.py
+++ CSCRPCAssociation/cscrpc_reco.py
@@ -4,7 +4,7 @@ process = cms.Process("demo")
 
 
 #Geometry
-from Configuration.Eras.Era_Phase2C8_timing_layer_bar_cff import Phase2C8_timing_layer_bar
+from Configuration.Eras.Era_Phase2C11I13M9_cff import Phase2C11I13M9
 process.load('Configuration.StandardSequences.GeometryRecoDB_cff')
 
 process.load("Geometry.MuonCommonData.muonIdealGeometryXML_cfi")
@@ -18,13 +18,13 @@ process.load("Geometry.MuonNumbering.muonNumberingInitialization_cfi")
 ## Global tag for 10_6 phase2 mc
 process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_cff')
 from Configuration.AlCa.GlobalTag import GlobalTag
-process.GlobalTag = GlobalTag(process.GlobalTag, '106X_upgrade2023_realistic_v3', '')
+process.GlobalTag = GlobalTag(process.GlobalTag, 'auto:phase2_realistic_T21', '')
 
 process.load("FWCore.MessageService.MessageLogger_cfi")
 process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(-1) )
 process.source = cms.Source("PoolSource",
 #   fileNames = cms.untracked.vstring('file:CSC_and_RPC.root')
-   fileNames = cms.untracked.vstring('file:L1T-PhaseIITDRSpring19DR-00054.root')
+   fileNames = cms.untracked.vstring('file:step2.root')
 )
 
 process.TFileService = cms.Service("TFileService",
@@ -34,9 +34,10 @@ process.TFileService = cms.Service("TFileService",
 
 
 process.demo = cms.EDAnalyzer('CSCRPCAssociation',
-   cscSegTag = cms.InputTag("cscSegments"),
-   rpcRecHitTag = cms.InputTag("rpcRecHits")
+   cscSegTag = cms.InputTag("cscSegments")
+   , rpcRecHitTag = cms.InputTag("rpcRecHits")
    , cscCorrDigisTag = cms.InputTag("simCscTriggerPrimitiveDigis")
+   , rpcDigisTag = cms.InputTag("simMuonRPCDigis")
 )
 
 process.p = cms.Path(process.demo)
