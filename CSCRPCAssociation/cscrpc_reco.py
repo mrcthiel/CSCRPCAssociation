a13fb0807c8aa586af380f0582ae3818f96ec940
jgomespi <65254882+jgomespi@users.noreply.github.com>
2021-07-05 20:58:36 -0300

    Update cscrpc_reco.py

--- CSCRPCAssociation/cscrpc_reco.py
+++ CSCRPCAssociation/cscrpc_reco.py
@@ -5,8 +5,8 @@ process = cms.Process("demo")
 
 #Geometry
 from Configuration.Eras.Era_Phase2C11I13M9_cff import Phase2C11I13M9
-process.load('Configuration.StandardSequences.GeometryRecoDB_cff')
-
+#process.load('Configuration.StandardSequences.GeometryRecoDB_cff')
+process.load('Configuration.Geometry.GeometryExtended2026D76Reco_cff')
 process.load("Geometry.MuonCommonData.muonIdealGeometryXML_cfi")
 process.load("FWCore.MessageService.MessageLogger_cfi")
 process.load("Geometry.RPCGeometry.rpcGeometry_cfi")
@@ -23,8 +23,9 @@ process.GlobalTag = GlobalTag(process.GlobalTag, 'auto:phase2_realistic_T21', ''
 process.load("FWCore.MessageService.MessageLogger_cfi")
 process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(-1) )
 process.source = cms.Source("PoolSource",
-#   fileNames = cms.untracked.vstring('file:CSC_and_RPC.root')
-   fileNames = cms.untracked.vstring('file:step2.root')
+#   fileNames = cms.untracked.vstring('file:step2.root')
+   fileNames = cms.untracked.vstring('file:/eos/home-j/jgomespi/workspace/MC_ForSummerStudents/step2.root')
+   ,duplicateCheckMode = cms.untracked.string('noDuplicateCheck')
 )
 
 process.TFileService = cms.Service("TFileService",
@@ -38,6 +39,7 @@ process.demo = cms.EDAnalyzer('CSCRPCAssociation',
    , rpcRecHitTag = cms.InputTag("rpcRecHits")
    , cscCorrDigisTag = cms.InputTag("simCscTriggerPrimitiveDigis")
    , rpcDigisTag = cms.InputTag("simMuonRPCDigis")
+   , wiresDigisTag = cms.InputTag("simMuonCSCDigis","MuonCSCWireDigi")
 )
 
 process.p = cms.Path(process.demo)
